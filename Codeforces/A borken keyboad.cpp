7c3386f150026cd8fc07d59f7a667280d17c7eff
ruhul <ruhul.cse7862@gmail.com>
2020-10-18 21:07:42 +0600

    broken keyboard an easy problem by div2

---
+++ Codeforces/A borken keyboad.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int test;
+  cin>>test;
+  while(test--){
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+      if(s[i]==s[i+1]){
+        s[i]='0';
+        s[i+1]='0';
+      }
+    }
+    set<char>ch;
+    for(auto i : s){
+      if(i!='0'){
+        ch.insert(i);
+      }
+    }
+   for(auto res : ch){
+     cout<<res;
+   }
+   cout<<endl;
+  }
+  return 0;
+}
