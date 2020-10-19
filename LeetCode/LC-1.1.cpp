cdb7802a32c24fa3a01a31bcfb86579743992392
ruhul <ruhul.cse7862@gmail.com>
2020-10-19 20:52:09 +0600

    Two sum solve using hash map

---
+++ LeetCode/LC-1.1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+     vector<int>res;
+  unordered_map<int,int>hash;
+  for(int i=0;i<nums.size();i++){
+    int f=target-nums.at(i);
+    if(hash.find(f)!=hash.end()){
+      res.push_back(hash[f]);
+      res.push_back(i);
+    }
+    hash[nums.at(i)]=i;
+  }
+return res;
+        
+    }
+};
