e9fa516959210d732ecb9b583a74344300efbde4
ruhul <ruhul.cse7862@gmail.com>
2020-10-22 23:22:21 +0600

    Binary search problem beginner

---
+++ Codeforces/Binary Search(A).cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int n,k;
+ cin>>n>>k;
+ vector<int>a(n);
+ for(int i=0;i<n;i++){
+   cin>>a[i];
+ }
+ for(int i=0;i<k;i++){
+   int x;
+   cin>>x;
+   int l=0;
+   int r=n-1;
+   bool ok=false;
+   while(r>=l){
+     int m=(l+r)/2;
+     if(a[m]==x){
+       ok=true;
+       break;
+     }
+     else if(a[m]<x){
+       l=m+1;
+     }
+     else{
+       r=m-1;
+     }
+   }
+   if(ok){
+     cout<<"YES"<<endl;
+   }
+   else{
+     cout<<"NO"<<endl;
+   }
+ }
+  return 0;
+}
