5a333ede24ab0365784a0dc819c7ec6abd09ead9
ruhul <ruhul.cse7862@gmail.com>
2020-10-22 23:19:48 +0600

    Binary search problem

---
+++ LeetCode/LC-704.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1,m;
+        while(l<=r){
+            m=(l+r)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            else if(nums[m]<target){
+                l=m+1;
+            }
+            else{
+                r=m-1;
+            }
+        }
+        return -1;
+    }
+};
