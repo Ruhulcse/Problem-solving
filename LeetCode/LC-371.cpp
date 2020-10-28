632755af4a7ea63049af754ac9a0b1627062fc79
ruhul <ruhul.cse7862@gmail.com>
2020-10-28 20:02:15 +0600

    sum of two integers solved by bit manipulation

---
+++ LeetCode/LC-371.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+       int c;
+        while(b!=0){
+            c=a&b;
+            a=a^b;
+            b= (unsigned int)(c)<<1;
+        }
+        return a;   
+    }
+};
