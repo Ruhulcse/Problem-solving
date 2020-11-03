a4f690165b5a0ed946b1e8292fec08f570744a30
Ruhul amin <ruhul.cse7862@gmail.com>
2020-11-03 12:02:13 +0600

    duplicate contains solved by hash map

---
+++ LeetCode/LC-217.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int>mp;
+        for(auto i:nums){
+            mp[i]++;
+            if(mp[i]>1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
