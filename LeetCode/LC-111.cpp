c024b5b41ec9b8d71a65e1e945acf30732295a31
Ruhul amin <ruhul.cse7862@gmail.com>
2020-12-02 12:11:58 +0600

    Minimum depth of binary Tree (Using bfs)

---
+++ LeetCode/LC-111.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        queue<TreeNode *>q;
+        q.push(root);
+        int result=0;
+        while(!q.empty()){
+            result++;
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode *p = q.front();
+                q.pop();
+                if(p->left==NULL&&p->right==NULL){
+                    return result;
+                }
+                if(p->left!=NULL){
+                    q.push(p->left);
+                }
+                if(p->right!=NULL){
+                    q.push(p->right);
+                }
+            }
+        }
+        return result;
+    }
+};
