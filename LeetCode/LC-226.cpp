00f7a900137382698e6006469c3310a522593dfe
Ruhul amin <ruhul.cse7862@gmail.com>
2020-12-07 12:15:20 +0600

    Invert Binary Tree (Recursion)

---
+++ LeetCode/LC-226.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void help(TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+        help(root->left);
+        help(root->right);
+        auto temp =root->left;
+        root->left=root->right;
+        root->right=temp;
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        help(root);
+        return root;
+        
+    }
+};
