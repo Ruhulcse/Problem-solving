b2c02a9021448651aa7dac031e8df27c045a6821
Ruhul amin <ruhul.cse7862@gmail.com>
2020-12-16 22:55:11 +0600

    Update LC-1.cpp

--- LeetCode/LC-1.cpp
+++ LeetCode/LC-1.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int tar) {
     vector<int>res;
     for(int i=0;i<nums.size()-1;i++){
     for(int j=i+1;j<nums.size();j++){
-      if(nums.at(i)+nums.at(j)==target){
+
+      if(nums.at(i)+nums.at(j)==tar){
        res.push_back(i);
           res.push_back(j);
       }
