6992cd727ebd893713b40a38ce23a1c54a9cf8bd
Ruhul amin <ruhul.cse7862@gmail.com>
2020-12-29 13:09:38 +0600

    Binary Tree Preorder Traversal(DFS)

---
+++ LeetCode/LC-144.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>result;
+    void DFS(TreeNode* root){
+        if(!root){
+            return;
+        }
+        result.push_back(root->val);
+        DFS(root->left);
+        DFS(root->right);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        DFS(root);
+        return result;
+    }
+};
