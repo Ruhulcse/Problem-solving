c5092a3bc778827a1e507e2e9c26a19402462b28
Ruhul amin <ruhul.cse7862@gmail.com>
2020-12-29 16:35:43 +0600

    Update LC-1302.cpp

---
+++ LeetCode/LC-1302.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        queue<TreeNode *>q;
+        q.push(root);
+        int sum=0;
+        while(!q.empty()){
+            int n=q.size();
+            
+            int lsum=0;
+            for(int i=0;i<n;i++){
+                TreeNode *p = q.front();
+                 q.pop();
+                 lsum+=p->val;
+                if(p->left!=NULL){
+                    q.push(p->left);
+                }
+                if(p->right!=NULL){
+                    q.push(p->right);
+                }
+            }
+            sum=lsum;
+        }
+        return sum;
+    }
+};
