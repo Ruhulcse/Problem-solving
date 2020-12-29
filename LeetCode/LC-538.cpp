9bce204f2d4b8e31ed9a653c476359b0a8b172f9
Ruhul amin <ruhul.cse7862@gmail.com>
2020-12-29 21:18:06 +0600

    BST to greater( just bfs)

---
+++ LeetCode/LC-538.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+      int sum=0;
+    void BFS(TreeNode* root){
+        if(!root){
+            return;
+        }
+        BFS(root->right);
+        root->val=root->val+sum;
+        sum=root->val;
+        BFS(root->left);
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        if(!root){
+            return NULL;
+        }
+        BFS(root);
+        return root;
+    }
+};
