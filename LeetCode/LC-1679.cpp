7f18188bb693ecd6ee556e29b43cf5c15a1a3302
Ruhul amin <ruhul.cse7862@gmail.com>
2021-01-19 17:23:03 +0600

    max number (Two pointer)

---
+++ LeetCode/LC-1679.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=nums.size()-1;
+        int cnt=0;
+        while(i<j){
+            if(nums[i]+nums[j]==k){
+                cnt++,i++,j--;
+            }
+            if(nums[i]+nums[j]>k){
+                j--;
+            }
+            if(nums[i]+nums[j]<k){
+                i++;
+            }
+        }
+        return cnt;
+    }
+};
