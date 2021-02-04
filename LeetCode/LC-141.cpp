eac9821606397757558b32d010a2c0fe43026880
Ruhul amin <ruhul.cse7862@gmail.com>
2021-02-04 13:28:12 +0600

    Linked List Cycle

---
+++ LeetCode/LC-141.cpp
@@ -0,0 +1,9 @@
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *fast=head, *slow=head;
+    while( slow && fast && fast->next ){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(fast==slow) return true;
+    }
+    return false;
+}
